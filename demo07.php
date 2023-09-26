a706750f796281df47dd486f9f9b5aa8d8bfe2b5
CasearF <syahxsfeng@gmail.com>
2023-09-26 18:02:05 +0800

    测试2

--- demo07.php
+++ demo07.php
@@ -18,7 +18,6 @@
     $xuehao = $_POST["xuehao"];
     $name = $_POST["name"];
     $banji = $_POST["banji"];
-    $banji = "2";
     #echo "$banji $name $xuehao";
     $shuzu = array($xuehao, $name, $banji);
     for ($i = 0; $i < count($shuzu); $i++) {
