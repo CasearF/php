525bb91e2b0e03d5b5957a6f3331690df8a0ba5c
CasearF <syahxsfeng@gmail.com>
2023-09-26 19:50:40 +0800

    aaa

--- demo07.php
+++ demo07.php
@@ -18,26 +18,27 @@
     $xuehao = $_POST["xuehao"];
     $name = $_POST["name"];
     $banji = $_POST["banji"];
+    $xuhao = 0;
     #echo "$banji $name $xuehao";
-    $shuzu = array($xuehao, $name, $banji);
-    for ($i = 0; $i < count($shuzu); $i++) {
-        #echo $shuzu[$i
-    }
-    ?>
+    $shuzu = array($xuhao, $xuehao, $name, $banji);
+    echo 
+    "    
     <table border=1px>
-        <tr>
-            <td class="a">序号</td>
-            <td class="a">学号</td>
-            <td class="a">姓名</td>
-            <td class="a">班级</td>
-        </tr>
-        <tr>
-
-            <td class="a"><?php echo $xuhao; ?></td>
-            <td class="a"><?php echo $xuehao; ?></td>
-            <td class="a"><?php echo $name; ?></td>
-            <td class="a"><?php echo $banji; ?></td>
-        </tr>
+    <tr>
+        <td class='a'>序号</td>
+        <td class='a'>学号</td>
+        <td class='a'>姓名</td>
+        <td class='a'>班级</td>
+    </tr>
+    ";
+    for ($i = 0; $i < count($shuzu); $i++) {
+        #echo "<td class='a'>" . ($i + 1) . "</td>";
+        echo "<td class='a'>" . $shuzu[$i] . "</td>";
+        $xuhao++;
+    }
+    echo "</tr>";
+    echo "</table>";
+    ?>
     </table>
 
 </body>
--- demo08.php
+++ demo08.php
@@ -1,11 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .a {
+            width: 150px;
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <?php
     $xuehao = $_POST["xuehao"];
     $name = $_POST["name"];
     $banji = $_POST["banji"];
-    $banji = "2";
-        #echo "$banji $name $xuehao";
     $shuzu = array($xuehao, $name, $banji);
+    echo "<table border=1px>
+        <tr>
+            <td class='a'>序号</td>
+            <td class='a'>学号</td>
+            <td class='a'>姓名</td>
+            <td class='a'>班级</td>
+        </tr>";
+    echo "<tr>";
     for ($i = 0; $i < count($shuzu); $i++) {
-    #    echo $shuzu[$i];
+        echo "<td class='a'>" . ($i + 1) . "</td>";
+        echo "<td class='a'>" . $shuzu[$i] . "</td>";
     }
+    echo "</tr>";
+    echo "</table>";
     ?>
+</body>
+</html>
