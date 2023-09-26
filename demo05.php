65e3f1c5b0213c5fdc94b886c06466b24b63e2aa
CasearF <syahxsfeng@gmail.com>
2023-09-26 11:19:26 +0800

    a

--- demo05.php
+++ demo05.php
@@ -1,10 +1,10 @@
 <?php
 $arr = array("中国","内蒙古","锡林郭勒职业学院","信息系人数","500");
 echo $arr[1];
-for ($i=0; $i <= 4; $i++) {
-    echo $arr[$i];
-    echo "<br>";
+for ($i=0; $i <= count($arr); $i++) {
+    echo $arr[$i]."<br>";
+    #echo "<br>";
 
 }
-echo count($arr);
+#echo count($arr);
 ?>
