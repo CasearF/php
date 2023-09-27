23ebcff301056754ca97fae2a2b1f7d58e9ae588
CasearF <syahxsfeng@gmail.com>
2023-09-27 15:40:37 +0800

    1

--- demo06.php
+++ demo06.php
@@ -1,4 +1,12 @@
 <!DOCTYPE html>
+/*
+ * @Author: CasearF syahxsfeng@gmail.com
+ * @Date: 2023-09-26 11:21:02
+ * @LastEditors: CasearF syahxsfeng@gmail.com
+ * @LastEditTime: 2023-09-27 15:40:24
+ * @FilePath: \anquan2201\demo06.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 <html lang="en">
 
 <head>
@@ -10,7 +18,8 @@
 <body>
     <form action="demo07.php" method="post">
         <label>学号</label>
-        <input type="text" name="xuehao">
+        <input type="tex
+        t" name="xuehao">
         <br>
         <label>名字</label>
         <input type="text" name="name">
@@ -19,6 +28,7 @@
         <input type="text" name="banji">
         <br>
         <input type="submit" value="计算">
+        <input type="reset" value="重置">
     </form>
 </body>
 
